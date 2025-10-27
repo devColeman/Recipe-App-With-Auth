c4f71ffcfa52c47c0046ed663d0a6003a6c38b30
Devin Coleman <croilders48@gmail.com>
2025-10-27 10:18:35 -0700

    working server

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+
+require('dotenv').config({path: './config/.env'})
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.listen(process.env.PORT, ()=> {
+    console.log('Server is running, you better catch it!')
+})
